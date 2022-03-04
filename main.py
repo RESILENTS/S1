323af8cc229f452c44426a82bf123b09ee86e93e
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-03-04 20:24:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def text(message):
     elif message.text == 'Рассылка' and chat_id in admins:
         message = bot.send_message(chat_id, '💁🏻‍♀️ Введите *сообщение* для рассылки', parse_mode="Markdown")
         bot.register_next_step_handler(message, add_message)
-    elif message.text == 'Кол-во пользователей' and chat_id in admins:
+    elif message.text == '📊 Статистика' and chat_id in admins:
         with sqlite3.connect('users.db') as conn:
             conn = sqlite3.connect('db.db')
             cursor = conn.cursor()
